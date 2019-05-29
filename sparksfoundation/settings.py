ac278b97f0fb50914544e5770f36efe749a5b861
arpit551 <imarpit03@gmail.com>
2019-05-30 01:29:29 +0530

    fix

--- sparksfoundation/settings.py
+++ sparksfoundation/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
